6158ba6a0d1a3ded6315efe53f261cee7a5781d3
jemis-maru <jemis.maru@bacancy.com>
2022-11-03 17:13:08 +0530

    Practicing template ref and content projection

--- src/app/component/custom-form/custom-form.component.ts
+++ src/app/component/custom-form/custom-form.component.ts
@@ -1,25 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit, Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-custom-form',
   templateUrl: './custom-form.component.html',
   styleUrls: ['./custom-form.component.scss']
 })
-export class CustomFormComponent implements OnInit {
+export class CustomFormComponent implements OnInit, AfterContentInit {
+
+  @ViewChild('userTypeRef', { static: true }) userTypeInput: any;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngAfterContentInit(): void {
+    console.log(this.userTypeInput);
+  }
+
   public userName: string = "";
   public userEmail: string = "";
-  public userType: string = "";
+  public userType: string = "Seller";
   public isFormSubmit: boolean = false;
   
 
   public userList: Array<string> = ['Admin', 'Seller', 'Buyer'];
-  public selectedUser:string = "rony";
+  public selectedUser:string = "Seller";
 
   public submitForm(): void{
     if(this.userEmail && this.userName){
